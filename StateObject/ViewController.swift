128a488a20f6b1669e0da7a29577a762cd0885ab
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-09-03 18:10:05 +0200

    Initial Commit

---
+++ StateObject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StateObject
+//
+//  Created by Manyuchi, Carrington C on 2024/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
